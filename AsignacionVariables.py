4d91a5694fb4a4edcec53a59addbbd0ca98b44d8
GrandMostach0 <Kreedlegend0@gmail.com>
2021-11-26 11:44:31 -0600

    Primer Comentario

---
+++ AsignacionVariables.py
@@ -0,0 +1,21 @@
+
+# <---- este simbolo que se señalo solo se usa para crear comentarios de solo una linea
+
+"""< - - - - - Inicio
+para crear comentarios multiples o comentarios que sean mas de una linea 
+ es necesario ponerle comillas dobles, ue indican que sea de varias linea y tambien nececesita de un fin de comillas double 
+ Final - - - - - >"""
+
+#asignacion de variables
+
+Nombre = "Minecraft"
+anio = 2011
+
+#para imprimir lo que se tiene guardado en la varibe solo es necesario ponerlo en un print(NombreDeLaVariable)
+
+print(Nombre)
+print(anio)
+print()#genera un espacio o salto de linea
+#para poder saber que tipo de variable es una variable es necesario ponerle el type(Nombre_De_La_Variable)
+print(type(Nombre))
+print(type(anio))
---
+++ HolaMundo.py
@@ -0,0 +1,8 @@
+
+# este es un comentario hecho en python de solo una sola linea
+
+#no es necesario crear un metodo main para que muestre todo los cambios como se hacia en java
+print("Hola Mundo") 
+print('Hola Mundo')
+print('"Hola Mundo"')
+print("'Hola Mundo'")
