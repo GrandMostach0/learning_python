d15109454eddc2371117083ecf0d46b5106cfdae
GrandMostach0 <Kreedlegend0@gmail.com>
2021-11-28 23:27:57 -0600

    ACTUALIZE ESTE ARCHIVO

--- AsignacionVariables.py
+++ AsignacionVariables.py
@@ -12,11 +12,22 @@ para crear comentarios multiples o comentarios que sean mas de una linea
 Nombre = "Zonacraft"
 anio = 2011
 
+"""Para crear una variable que sea constante o indicar que una variable es constane solo basta con escribir el nombre de la variable
+en MAYUSCULAS totalmente ej: (abajo)"""
+CONSTANTE = 9.84
+
+"""En python y como en todos los lenguajes de programacion existe reglas para indicar o crear variables y una de esas reglas mas basicas o 
+la mas comun es que no se puede crear variables con nombre de palabras reservadas"""
+
+
 #para imprimir lo que se tiene guardado en la varibe solo es necesario ponerlo en un print(NombreDeLaVariable)
 
 print(Nombre)
 print(anio)
+print(CONSTANTE)
 print()#genera un espacio o salto de linea
 #para poder saber que tipo de variable es una variable es necesario ponerle el type(Nombre_De_La_Variable)
 print(type(Nombre))
 print(type(anio))
+print(type(CONSTANTE)) #aqui obviamente no mostrara o indicara que es constante pero todo programador tiene que saber que en todo lenguaje para indicar constante
+#es escribir el nombre de la variable con MAYUSCULAS
